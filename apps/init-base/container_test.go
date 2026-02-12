dfe57b2849b30a009dd852fe1c03d76c8b7c3f92
charming-byte <13345912+charming-byte@users.noreply.github.com>
2026-02-12 08:09:47 +0100

    fix: container_test

--- apps/init-base/container_test.go
+++ apps/init-base/container_test.go
@@ -9,6 +9,6 @@ import (
 
 func Test(t *testing.T) {
 	ctx := context.Background()
-	image := testhelpers.GetTestImage("ghcr.io/charming-byte/containers/init-base:alpine-3.23")
+	image := testhelpers.GetTestImage("ghcr.io/charming-byte/init-base:alpine-3.23")
 	testhelpers.TestCommandSucceeds(t, ctx, image, nil, "/usr/bin/bash")
 }
